83a81b24d11c93508ffa0eab351f4ab58bc84dda
eliel <silvaeliel940@gmail.com>
2022-04-26 16:17:39 -0300

    feat requisito 3 feito mais tem que ser revisto pois é necessario pra outro requisito mais a frente

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <label data-testid="home-initial-message" htmlFor="mainSearch">
-      Digite algum termo de pesquisa ou escolha uma categoria.
-      <input id="mainSearch" type="text" />
-    </label>
+    <div>
+      <label data-testid="home-initial-message" htmlFor="mainSearch">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+        <input id="mainSearch" type="text" />
+      </label>
+      <Link to="/shopingcart" data-testid="shopping-cart-button">
+        <input type="button" value="cart" />
+      </Link>
+    </div>
   );
 }
 

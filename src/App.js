b396fe3686158652f8bd2f6f3648dd6e6168f66c
Caio Mendes <cacammoreno@gmail.com>
2022-04-26 18:19:42 -0300

    fix: organização das pastas

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
-import ShopingCart from './pages/ShopingCart';
+import ShoppingCart from './pages/ShoppingCart';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={ Home } />
-        <Route path="/shopingcart" component={ ShopingCart } />
+        <Route path="/shopingcart" component={ ShoppingCart } />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/ShoppingCart/index.jsx
+++ src/pages/ShoppingCart/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class ShopingCart extends Component {
+export default class ShoppingCart extends Component {
   render() {
     return (
       <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
